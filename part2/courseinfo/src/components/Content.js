999083b7adddb05eac31c8da7f8721fb8d504fdc
Mihail Badoiu <badoiumihail@gmail.com>
2023-04-16 19:59:44 +0300

    part2-a exercises 2.1 through 2.5 - courseinfo

---
+++ part2/courseinfo/src/components/Content.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Part from "./Part";
+import Total from "./Total";
+
+export default function Content({ parts }) {
+    return (
+        <>
+            <ul>
+                {parts.map((part) => (
+                    <Part
+                        key={part.id}
+                        name={part.name}
+                        exercises={part.exercises}
+                    />
+                ))}
+            </ul>
+            <Total parts={parts} />
+        </>
+    );
+}
