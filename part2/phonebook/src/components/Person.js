02c1ffe757d523ded4f22edb9ecb8b1d61e7b904
Mihail Badoiu <badoiumihail@gmail.com>
2023-04-17 23:17:27 +0300

    part2-d exercises 2.12 through 2.15 - phonebook

--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-export default function Person({ name, phone }) {
+export default function Person({ name, phone, id, handleClick }) {
     return (
         <>
             <li>
                 {name} {phone}
             </li>
+            <button onClick={handleClick(id)} type="submit">
+                Delete
+            </button>
         </>
     );
 }
